b02d3b7cf880f656deeb6adf37dfad6e1d327ff5
PatrickHickey <patrickstephenhickey@gmail.com>
2018-01-26 04:12:35 -0800

    WeatherApp

--- WeatherApp/src/containers/weather_list.js
+++ WeatherApp/src/containers/weather_list.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Chart data={temps} color="red" />
+        </td>
+      </tr>
+    );
+  }
+
   render() {
-    return(
+    return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
   }
 }
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
