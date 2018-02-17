8d480e914a8406a0e79633c21c981e6e75d9922a
PatrickHickey <patrickstephenhickey@gmail.com>
2018-02-17 04:18:35 -0800

    Testing

--- Testing/test/components/comment_list_test.js
+++ Testing/test/components/comment_list_test.js
@@ -2,5 +2,19 @@ import { renderComponent, expect } from '../test_helper';
 import CommentList from '../../src/components/comment_list';
 
 describe ('CommentList', () => {
+  let component;
 
+  beforeEach(() => {
+    const props = { comments: ['Comment One', 'Comment Two'] }
+    component = renderComponent(CommentList, null, props);
+  });
+
+  it('shows an LI for each comment', () => {
+    expect(component.find('li').length).to.equal(2);
+  });
+
+  it('shows each comment that is provided', () => {
+    expect(component).to.contain('Comment One');
+    expect(component).to.contain('Comment Two');
+  });
 });
