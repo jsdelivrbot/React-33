949c9f1cfa523c9d1e64705ba6d445da1dd2d6ee
PatrickHickey <patrickstephenhickey@gmail.com>
2018-02-19 03:05:33 -0800

    Hoc

---
+++ Hoc/src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Top Secret Files
+      <ul>
+        <li>Doc One</li>
+        <li>Doc Two</li>
+        <li>Doc Three</li>
+      </ul>
+    </div>
+  );
+};
