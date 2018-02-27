27ecc1cefb4ba5598efd0ad8f369225bb4b7321b
PatrickHickey <patrickstephenhickey@gmail.com>
2018-02-27 04:59:50 -0800

    auth

--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Define out model
+// Define our model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
