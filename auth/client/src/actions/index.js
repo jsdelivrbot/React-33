3b6f3374b153a8e2d220d46b6dc53e9280bba1b3
PatrickHickey <patrickstephenhickey@gmail.com>
2018-03-02 04:57:11 -0800

    auth

---
+++ auth/client/src/actions/index.js
@@ -0,0 +1,15 @@
+
+
+export function signinUser({ email, password }) {
+  return function(dispatch) {
+    // Submit email/apssword to server
+
+    // If request is good...
+    // - Update state to indicate user is authorized
+    // - Save the JWT token
+    // - redirect to the route '/feature'
+
+    // If request is bad...
+    // - Show an error to the user
+  }
+}
