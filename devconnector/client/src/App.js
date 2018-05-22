77d7cfd00f0c08ff72d45e25ececb48f0f4bb5cd
PatrickHickey <patrickstephenhickey@gmail.com>
2018-05-22 02:32:44 -0700

    Added TextFieldGroup

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -28,7 +28,6 @@ if (localStorage.jwtToken) {
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     // Logout user
-    let logoutUser = store.dispatch.logoutUser;
     store.dispatch(logoutUser());
     // Clear current Profile
 
